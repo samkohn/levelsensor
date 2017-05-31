d8975ba2bc7c1fb35552d2c1a0b3e2fca4288055
Sam Kohn <kohn@berkeley.edu>
2017-05-31 12:35:40 -0700

    Update log program: CL args, reset serial connection periodically

--- log_arduino.py
+++ log_arduino.py
@@ -1,10 +1,19 @@
 import time
 import serial
-s = serial.Serial('/dev/cu.usbmodem1411', timeout=0)
-fileout = open('datalog-height-adjust.txt','w')
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--output', help='Output log file name')
+parser.add_argument('--port', help='address of serial port to read')
+args = parser.parse_args()
+fileout = open(args.output,'w')
 while True:
-    output = s.readline()
-    if len(output) > 0:
-        fileout.write(output + '\n')
-        print output
-    time.sleep(1)
+    # Weird serial read issue (duplicated across software)
+    # causes us to close and reopen serial port once per minute.
+    with serial.Serial(args.port, timeout=0) as s:
+        for i in xrange(60):
+            output = s.readline()
+            if len(output) > 0 and i != 0:
+                fileout.write(output)
+                print output
+            time.sleep(1)
